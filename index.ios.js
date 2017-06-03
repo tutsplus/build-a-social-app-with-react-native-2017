aa10c52ad8acaec46f4291b2a59337a1af3f9685
Markus Mühlberger <markus@mmuehlberger.com>
2017-06-03 11:57:27 +0200

    Add ProfileScreen and react-native-elements

--- index.ios.js
+++ index.ios.js
@@ -12,6 +12,8 @@ import {
   View
 } from 'react-native';
 
+import ProfileScreen from './profile';
+
 export default class PictureFeed extends Component {
   render() {
     return (
@@ -50,4 +52,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('PictureFeed', () => PictureFeed);
+AppRegistry.registerComponent('PictureFeed', () => ProfileScreen);
